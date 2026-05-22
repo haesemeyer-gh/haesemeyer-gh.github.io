ebde3757ee4a3526ece13dd610c90b37b4c71831
haesemeyer <haesemeyer@mailbox.org>
2026-05-22 10:18:38 +0000

    animationiteration
    use animatiniteration instead of interval so the animation doesn't jump

--- scripts/katzenbeschleunigungsanlage.js
+++ scripts/katzenbeschleunigungsanlage.js
@@ -2,11 +2,11 @@ const katze = document.getElementById('katze').firstElementChild;
 const explosion = document.getElementById('katze').lastElementChild;
 
 let speed = 4;
-setInterval(() => {
+katze.addEventListener("animationiteration", () => {
     if (speed > 0.7) {
         speed -= 0.001;
         katze.style.animationDuration = `${speed}s`;
     } else {
         explosion.classList.remove("display-none");
     }
-}, 1000);
+});
