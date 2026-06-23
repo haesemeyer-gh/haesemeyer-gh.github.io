87c50fc20ec92b5f238ea0dcbb3dda85ae268082
haesemeyer <haesemeyer@mailbox.org>
2026-06-23 16:31:50 +0200

    zeigestock fixes

--- scripts/zeigestock.js
+++ scripts/zeigestock.js
@@ -1,15 +1,18 @@
 /* Canvas */
 
 var canvas = document.createElement('canvas');
-canvas.height = window.innerHeight;
-canvas.width = window.innerWidth;
 canvas.style.position = "fixed";
-canvas.style.height = "100vh";
-canvas.style.width = "100%";
 canvas.style.top = 0;
 canvas.style.left = 0;
 canvas.style.pointerEvents = "none";
 canvas.style.display = "none";
+
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+window.addEventListener('resize', () => {
+	canvas.height = window.innerHeight;
+	canvas.width = window.innerWidth;
+});
 
 document.body.appendChild(canvas);
 
@@ -23,21 +26,22 @@ zeigestock.style.objectFit = "cover";
 var ctx = canvas.getContext('2d');
 
 function drawZeigestock(angle, mouseX, mouseY) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.save();
-    ctx.translate(canvas.width, canvas.height);
-    ctx.rotate((Math.PI/2)+0.02);
-    ctx.rotate(angle);
-    zeigestock.height = Math.sqrt( Math.pow((canvas.height - mouseY), 2) + Math.pow((canvas.width - mouseX), 2) );
-    ctx.drawImage(zeigestock, -zeigestock.width, -zeigestock.height, );
-    ctx.restore();
+	ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+	ctx.save();
+	ctx.translate(window.innerWidth, window.innerHeight);
+	ctx.rotate((Math.PI/2)+0.02);
+	ctx.rotate(angle);
+	zeigestock.height = Math.sqrt( Math.pow((mouseY), 2) + Math.pow((mouseX), 2) );
+	ctx.drawImage(zeigestock, -zeigestock.width, -zeigestock.height, );
+	ctx.restore();
 }
 
 document.onmousemove = function(e) {
-    var dx = e.pageX - canvas.width;
-    var dy = e.pageY - canvas.height;
-    var theta = Math.atan2(dy, dx);
-    drawZeigestock(theta, e.pageX, e.pageY);
+	console.log(e);
+	var dx = e.clientX - window.innerWidth;
+	var dy = e.clientY - window.innerHeight;
+	var theta = Math.atan2(dy, dx);
+	drawZeigestock(theta, dx, dy);
 };
 
 /* Menu */
@@ -48,7 +52,7 @@ zeigestockToggleText.innerText = "Zeigestock: ";
 const zeigestockToggleBox = document.createElement('input');
 zeigestockToggleBox.type = "checkbox";
 zeigestockToggleBox.addEventListener('input', () => {
-    canvas.style.display = (zeigestockToggleBox.checked ? "block" : "none");
+	canvas.style.display = (zeigestockToggleBox.checked ? "block" : "none");
 });
 
 const zeigestockToggle = document.createElement('label');
@@ -56,9 +60,10 @@ zeigestockToggle.append(zeigestockToggleText, zeigestockToggleBox);
 
 const zeigestockMenu = document.createElement('div');
 zeigestockMenu.ariaHidden = true;
-zeigestockMenu.style.position = "absolute";
+zeigestockMenu.style.position = "fixed";
 zeigestockMenu.style.bottom = 0;
 zeigestockMenu.style.right = 0;
 zeigestockMenu.appendChild(zeigestockToggle);
 
 document.body.appendChild(zeigestockMenu);
+
