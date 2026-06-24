eda273a7c3b4bccd80f911ca5d9e3a1bdc918c84
haesemeyer <haesemeyer@mailbox.org>
2026-06-24 08:27:40 +0200

    zeigestock update
    - alles ist nun IIFE
    - z-index wurde gesetzt

--- scripts/zeigestock.js
+++ scripts/zeigestock.js
@@ -1,85 +1,90 @@
-/* Canvas */
+(() => {
 
-// Create Canvas
-var canvas = document.createElement('canvas');
-canvas.style.position = "fixed";
-canvas.style.top = 0;
-canvas.style.left = 0;
-canvas.style.display = "none";
-// allow clicking through canvas, disable this so you can't click anything anymore
-canvas.style.pointerEvents = "none";
+    /* Canvas */
 
-// Canvas is always the size of the viewport
-canvas.height = window.innerHeight;
-canvas.width = window.innerWidth;
-// allow resizing window without having to reload page
-window.addEventListener('resize', () => {
-	canvas.height = window.innerHeight;
-	canvas.width = window.innerWidth;
-});
+    // Create Canvas
+    let canvas = document.createElement('canvas');
+    canvas.style.position = "fixed";
+    canvas.style.top = 0;
+    canvas.style.left = 0;
+    canvas.style.zIndex = 123450;
+    canvas.style.display = "none";
+    // allow clicking through canvas, disable this so you can't click anything anymore
+    canvas.style.pointerEvents = "none";
 
-document.body.appendChild(canvas);
+    // Canvas is always the size of the viewport
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    // allow resizing window without having to reload page
+    window.addEventListener('resize', () => {
+        canvas.height = window.innerHeight;
+        canvas.width = window.innerWidth;
+    });
 
-// Zeigestock Image Definition
-var zeigestock = new Image();
-zeigestock.onload = function() {
-	drawZeigestock(0);
-};
-zeigestock.src = "https://haesemeyer.dev/assets/zeigestock.png";
-zeigestock.style.objectFit = "cover";
+    document.body.appendChild(canvas);
 
-var ctx = canvas.getContext('2d');
+    // Zeigestock Image Definition
+    let zeigestock = new Image();
+    zeigestock.onload = function() {
+        drawZeigestock(0);
+    };
+    zeigestock.src = "https://haesemeyer.dev/assets/zeigestock.png";
+    zeigestock.style.objectFit = "cover";
 
-// Function to draw Zeigestock on the Canvas
-function drawZeigestock(angle, mouseX, mouseY) {
-	ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-	ctx.save();
-	ctx.translate(window.innerWidth, window.innerHeight);
-	ctx.rotate((Math.PI/2)+0.02);
-	ctx.rotate(angle);
-	zeigestock.height = Math.sqrt( Math.pow((mouseY), 2) + Math.pow((mouseX), 2) );
-	ctx.drawImage(zeigestock, -zeigestock.width, -zeigestock.height, );
-	ctx.restore();
-}
+    let ctx = canvas.getContext('2d');
 
-// Mouse Move Event
-document.onmousemove = function(e) {
-	var dx = e.clientX - window.innerWidth;
-	var dy = e.clientY - window.innerHeight;
-	var theta = Math.atan2(dy, dx);
-	drawZeigestock(theta, dx, dy);
-};
+    // Function to draw Zeigestock on the Canvas
+    function drawZeigestock(angle, mouseX, mouseY) {
+        ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        ctx.save();
+        ctx.translate(window.innerWidth, window.innerHeight);
+        ctx.rotate((Math.PI/2)+0.02);
+        ctx.rotate(angle);
+        zeigestock.height = Math.sqrt( Math.pow((mouseY), 2) + Math.pow((mouseX), 2) );
+        ctx.drawImage(zeigestock, -zeigestock.width, -zeigestock.height, );
+        ctx.restore();
+    }
 
-/* Menu */
+    // Mouse Move Event
+    document.onmousemove = function(e) {
+        let dx = e.clientX - window.innerWidth;
+        let dy = e.clientY - window.innerHeight;
+        let theta = Math.atan2(dy, dx);
+        drawZeigestock(theta, dx, dy);
+    };
 
-const zeigestockToggleText = document.createElement('span');
-zeigestockToggleText.innerText = "Zeigestock: ";
+    /* Menu */
 
-const zeigestockToggleBox = document.createElement('input');
-zeigestockToggleBox.type = "checkbox";
-zeigestockToggleBox.addEventListener('input', () => {
-	// Toggle Canvas Visibility
-	canvas.style.display = (zeigestockToggleBox.checked ? "block" : "none");
-	// Toggle Hidden Cursor
-	if (zeigestockToggleBox.checked) {
-		const hiddenCursor = document.createElement('style');
-		hiddenCursor.id = 'zeigestock-hidden-cursor-style';
-		hiddenCursor.textContent = "* { cursor: none !important; }";
-		document.head.appendChild(hiddenCursor);
-	} else {
-		document.getElementById('zeigestock-hidden-cursor-style').remove();
-	}
-});
+    let zeigestockToggleText = document.createElement('span');
+    zeigestockToggleText.innerText = "Zeigestock: ";
 
-const zeigestockToggle = document.createElement('label');
-zeigestockToggle.append(zeigestockToggleText, zeigestockToggleBox);
+    let zeigestockToggleBox = document.createElement('input');
+    zeigestockToggleBox.type = "checkbox";
+    zeigestockToggleBox.addEventListener('input', () => {
+        // Toggle Canvas Visibility
+        canvas.style.display = (zeigestockToggleBox.checked ? "block" : "none");
+        // Toggle Hidden Cursor
+        if (zeigestockToggleBox.checked) {
+            let hiddenCursor = document.createElement('style');
+            hiddenCursor.id = 'zeigestock-hidden-cursor-style';
+            hiddenCursor.textContent = "* { cursor: none !important; }";
+            document.head.appendChild(hiddenCursor);
+        } else {
+            document.getElementById('zeigestock-hidden-cursor-style').remove();
+        }
+    });
 
-const zeigestockMenu = document.createElement('div');
-zeigestockMenu.ariaHidden = true;
-zeigestockMenu.style.position = "fixed";
-zeigestockMenu.style.bottom = 0;
-zeigestockMenu.style.right = 0;
-zeigestockMenu.appendChild(zeigestockToggle);
+    let zeigestockToggle = document.createElement('label');
+    zeigestockToggle.append(zeigestockToggleText, zeigestockToggleBox);
 
-document.body.appendChild(zeigestockMenu);
+    let zeigestockMenu = document.createElement('div');
+    zeigestockMenu.ariaHidden = true;
+    zeigestockMenu.style.position = "fixed";
+    zeigestockMenu.style.bottom = 0;
+    zeigestockMenu.style.right = 0;
+    zeigestockMenu.style.zIndex = 123451;
+    zeigestockMenu.appendChild(zeigestockToggle);
 
+    document.body.appendChild(zeigestockMenu);
+
+})();
